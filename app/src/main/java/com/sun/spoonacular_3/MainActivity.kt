93686805fd27bbfff7b7ed839f06b4f31c12f416
phong10101998 <phongphongphong10101998@gmail.com>
2021-03-15 16:02:08 +0700

    init project

---
+++ app/src/main/java/com/sun/spoonacular_3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sun.spoonacular_3.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sun.spoonacular_3.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
